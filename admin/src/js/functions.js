400269c4505104c04b3176f4c508155b50d6caf1
Anatoly Batov <play.hoffa@gmail.com>
2024-12-06 15:23:31 +0400

    functions.js

---
+++ admin/src/js/functions.js
@@ -0,0 +1,29 @@
+export function dispatchUpdateEvent(arg) {
+    const event = new CustomEvent("updateHall", {
+        detail: arg
+    });
+    document.querySelector(".main").dispatchEvent(event);
+
+    document.querySelector(".main").addEventListener("updateHall", (event) => {
+        console.log("Данные события:", event.detail);
+    });
+}
+
+export async function getHalls(activHallId = null) {
+    const token = localStorage.getItem('token');
+    try {
+        const jsonResponse = await fetch(`${_URL}hall`, {
+            method: "GET",
+            headers: { Autorization: `Bearer ${token}` },
+        });
+        const response = await jsonResponse.json();
+        if (!response.length) {
+            return console.log("Нет данных о залах, массив пуст");
+        }
+        if (!activHallId) {
+            activHallId = response[0].id;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
