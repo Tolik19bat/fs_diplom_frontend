1972ba412d141fb898b7e4dceab6a2e12f5c6baf
Anatoly Batov <play.hoffa@gmail.com>
2024-12-08 14:33:41 +0400

    defaultHallData, HallManagement, Page

---
+++ admin/src/js/defaultHallData.js
@@ -0,0 +1,24 @@
+// const defaultHallData = [
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+//   [1, 1, 1, 1, 1, 1, 1, 1],
+// ];
+
+// 0 - заблокированные (нет кресла)
+// 1 - обычные кресла
+// 2 - VIP кресла
+
+const defaultRows = 10;
+const defaultChairsInRow = 10;
+
+const ticketPrice = 250;
+const vipTicketPrice = 500;
+
+export { defaultRows, defaultChairsInRow, ticketPrice, vipTicketPrice };
