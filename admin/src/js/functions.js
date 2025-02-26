1eb418664d1b36ec0c78d4db16bca9ace8cd144b
Anatoly Batov <play.hoffa@gmail.com>
2025-02-26 05:41:22 +0400

    functions

--- admin/src/js/functions.js
+++ admin/src/js/functions.js
@@ -6,19 +6,19 @@ export function dispatchUpdateEvent(arg) {
   const event = new CustomEvent("updateHall", {
     detail: arg, // Внутреннее свойство 'detail' содержит переданные данные
   });
-  
+
   // Находим элемент с классом '.main' и отправляем на него событие
   document.querySelector(".main").dispatchEvent(event);
 
   // Добавляем обработчик события 'updateHall' на элемент '.main'
-  document.querySelector(".main").addEventListener("updateHall", (event) => {
-    // Когда событие 'updateHall' сработает, выводим данные из события в консоль
-    console.log("Данные события:", event.detail);
-  });
+  // document.querySelector(".main").addEventListener("updateHall", (event) => {
+  //   // Когда событие 'updateHall' сработает, выводим данные из события в консоль
+  //   console.log("Данные события:", event.detail);
+  // });
 }
 
 // Асинхронная функция для получения данных о залах
-export async function getHalls(activHallId = null) {
+export async function getHalls(activeHallId = null) {
   // Получаем токен авторизации из localStorage
   const token = localStorage.getItem("token");
 
@@ -28,19 +28,19 @@ export async function getHalls(activHallId = null) {
       method: "GET", // Метод запроса - GET
       headers: { Authorization: `Bearer ${token}` }, // Заголовок Authorization для передачи токена
     });
-    
+
     // Преобразуем ответ в формат JSON
     const response = await jsonResponse.json();
 
-    // Проверяем, если массив залов пуст, выводим сообщение в консоль
-    if (!response.length) {
-      return console.log("Нет данных о залах, массив пуст");
-    }
-
     // Если id активного зала не передан, то выбираем первый зал из ответа
-    if (!activHallId) {
-      activHallId = response[0].id;
+    if (response.length > 0 && !activeHallId) {
+      activeHallId = response[0].id;
     }
+    // Отправляем событие обновления с переданными данными
+    dispatchUpdateEvent({
+      data: response, // Передаём полученные данные
+      activeHallId, // Передаём ID активного зала, чтобы обновить его состояние
+    });
   } catch (error) {
     // В случае ошибки выводим её в консоль
     console.log(error);
