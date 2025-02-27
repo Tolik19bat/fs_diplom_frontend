371a0d3f2d0f3d1594ada504092bcb7bdf6be184
Anatoly Batov <play.hoffa@gmail.com>
2025-02-27 05:00:25 +0400

    Page

--- admin/src/js/Page.js
+++ admin/src/js/Page.js
@@ -1,7 +1,8 @@
 // Импортируем зависимости для управления разными частями приложения
 import { _URL } from "./app.js";
+import Fetch from "./Fetch.js";
+import Loader from "./Loader.js";
 //import accordeon from "./accordeon.js"; // Модуль для аккордеона
-import { getHalls } from "./functions.js"; // Функция для получения данных о залах
 import HallManagement from "./HallManagement.js"; // Управление залами
 import HallConfiguration from "./HallConfiguration.js"; // Конфигурация залов
 import PriceConfiguration from "./PriceConfiguration.js"; // Конфигурация цен
@@ -42,18 +43,20 @@ export default class Page {
   }
 // Асинхронный метод для загрузки залов с сервера
 async getHalls() {
-  try {
-    // Отправляем GET-запрос к API для получения списка залов
-    const jsonResponse = await fetch(`${_URL}hall`, {
-      method: "GET", // Метод запроса
-      headers: { Authorization: `Bearer ${token}` }, // Передаём токен авторизации в заголовках
-    });
+  return await Fetch.send("GET", "hall"); // Отправляем асинхронный GET-запрос на сервер для получения списка залов
 
-    // Преобразуем ответ в JSON и возвращаем результат
-    return await jsonResponse.json();
-  } catch (error) {
-    // Если произошла ошибка во время запроса, выводим её в консоль
-    console.error("Ошибка при загрузке залов:", error);
-  }
+  // try {
+  //   // Отправляем GET-запрос к API для получения списка залов
+  //   const jsonResponse = await fetch(`${_URL}hall`, {
+  //     method: "GET", // Метод запроса
+  //     headers: { Authorization: `Bearer ${token}` }, // Передаём токен авторизации в заголовках
+  //   });
+
+  //   // Преобразуем ответ в JSON и возвращаем результат
+  //   return await jsonResponse.json();
+  // } catch (error) {
+  //   // Если произошла ошибка во время запроса, выводим её в консоль
+  //   console.error("Ошибка при загрузке залов:", error);
+  // }
 }
 }
