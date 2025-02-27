5f688a6357cc7ca4c2031b16ab6131a7f46774ed
Anatoly Batov <play.hoffa@gmail.com>
2025-02-27 06:10:05 +0400

    Fetch

--- client/src/js/Fetch.js
+++ client/src/js/Fetch.js
@@ -2,36 +2,54 @@ import { _URL } from "./app.js";
 import Loader from "./Loader.js";
 
 export default class Fetch {
+  // Создаем класс Fetch для выполнения HTTP-запросов
   static async send(method, query, options = {}) {
-    Loader.startLoader();
+    // Статический метод send для отправки запросов
+    Loader.startLoader(); // Запускаем индикатор загрузки перед выполнением запроса
     try {
       const requestOptions = {
-        method,
-        headers: {},
+        // Формируем объект с параметрами запроса
+        method, // HTTP-метод (GET, POST, PUT, DELETE и т. д.)
+        headers: {}, // Заголовки запроса (по умолчанию пустые)
       };
+
+      // Если передан formData, добавляем его в тело запроса
       if (options?.formData) {
         requestOptions.body = options.formData;
       }
+
+      // Если передан bodyJson, сериализуем его в JSON и устанавливаем соответствующий заголовок
       if (options?.bodyJson) {
         requestOptions.body = JSON.stringify(options.bodyJson);
         requestOptions.headers["Content-Type"] = "application/json";
       }
+
+      // Выполняем HTTP-запрос с указанным URL и опциями
       const response = await fetch(_URL + query, requestOptions);
+
+      // Если указано cleanResponse, возвращаем "сырой" Response-объект без обработки
       if (options?.cleanResponse) {
-        Loader.stopLoader();
+        Loader.stopLoader(); // Останавливаем индикатор загрузки
         return response;
       }
+
+      // Если сервер вернул JSON-ответ, парсим его и возвращаем
       if (response.headers.get("content-type") === "application/json") {
         Loader.stopLoader();
         return await response.json();
       }
+
+      // Если сервер вернул текстовый ответ, читаем его и возвращаем
       if (response.headers.get("content-type").includes("text/plain")) {
         Loader.stopLoader();
         return await response.text();
       }
+
+      // Останавливаем лоадер в случае, если предыдущие проверки не сработали
       Loader.stopLoader();
     } catch (error) {
-      console.error(error);
+      // Обрабатываем возможные ошибки при выполнении запроса
+      console.error(error); // Выводим ошибку в консоль
     }
   }
 }
