467c39867019b53200d9295940e3ca28f9323758
Anatoly Batov <play.hoffa@gmail.com>
2025-02-27 04:06:05 +0400

    Login

--- admin/src/js/Login.js
+++ admin/src/js/Login.js
@@ -1,4 +1,5 @@
 import { _URL, _URL_ADMIN_INDEX } from "./app.js";
+import Fetch from "./Fetch.js";
 
 // Определяем класс Login для управления логикой входа
 export default class Login {
@@ -16,10 +17,15 @@ export default class Login {
   bindToDom() {
     this.containerEl = document.querySelector("main"); // Находим главный контейнер
     this.loginFormEl = document.querySelector(".login__form"); // Находим форму входа
-    this.loginFormEl.addEventListener("submit", this.onSubmitLoginForm.bind(this)); 
+    this.loginFormEl.addEventListener(
+      "submit",
+      this.onSubmitLoginForm.bind(this)
+    );
     // Привязываем обработчик события отправки формы
     this.loginInputEmailEl = document.querySelector(".login__input_email"); // Поле для ввода email
-    this.loginInputPasswordEl = document.querySelector(".login__input_password"); // Поле для ввода пароля
+    this.loginInputPasswordEl = document.querySelector(
+      ".login__input_password"
+    ); // Поле для ввода пароля
   }
 
   onSubmitLoginForm(e) {
@@ -39,33 +45,47 @@ export default class Login {
     localStorage.setItem("token", token); // Сохраняем токен в локальное хранилище
   }
 
+  /**
+   * Функция отправляет данные авторизации
+   *
+   * @async
+   * @returns {*}
+   */
   async sendForm() {
-    try {
-      // Отправляем запрос на сервер для авторизации
-      const jsonResponse = await fetch(`${_URL}login`, {
-        method: "POST", // Метод POST для отправки данных
-        headers: {
-          "Content-Type": "application/json", // Тип содержимого
+    const jsonResponse = await Fetch.send( // Отправляем асинхронный POST-запрос и сохраняем ответ в переменную `jsonResponse`
+      "POST", // Указываем метод запроса - POST (отправка данных)
+      "tokens/create", // Указываем URL эндпоинта, который обрабатывает создание токена
+      {
+        cleanResponse: true, // Возможно, указывает серверу вернуть "чистый" ответ без обертки
+        bodyJson: { // Передаем тело запроса в формате JSON
+          email: this.loginInputEmailEl.value, // Значение поля email из формы авторизации
+          password: this.loginInputPasswordEl.value, // Значение поля password из формы авторизации
+          device_name: "windows", // Указываем имя устройства для идентификации
         },
-        body: JSON.stringify({
-          email: this.loginInputEmailEl.value, // Передаем email из поля ввода
-          password: this.loginInputPasswordEl.value, // Передаем пароль из поля ввода
-          device_name: "windows", // Указываем устройство
-        }),
-      });
-
-      if (jsonResponse.ok) {
-        const response = await jsonResponse.json(); // Получаем ответ от сервера
-        this.putTokenIntoLocalStorage(response.token); // Сохраняем токен в локальное хранилище
-        window.location.href = _URL_ADMIN_INDEX; // Перенаправляем на страницу администратора
-      } else {
-        const errorResponse = await jsonResponse.json(); // Получаем ошибку
-        console.log(errorResponse); // Логируем ошибку
-        alert("Ошибка авторизации. Проверьте данные и попробуйте снова."); // Выводим сообщение об ошибке
       }
-    } catch (error) {
-      console.log(error); // Логируем ошибку сети
-      alert("Произошла ошибка. Попробуйте снова позже."); // Выводим сообщение о сетевой ошибке
+    );
+    if (jsonResponse.ok) { // Проверяем, успешно ли выполнен запрос (статус ответа 200-299)
+      const response = await jsonResponse.json(); // Асинхронно парсим JSON-ответ от сервера и сохраняем в переменную `response`
+      this.putTokenIntoLocalStorage(response.token); // Вызываем метод, который сохраняет полученный токен в локальное хранилище
+      window.location.href = _URL_ADMIN_INDEX; // Перенаправляем пользователя на страницу администратора
     }
+    // try {
+    //   // Отправляем запрос на сервер для авторизации
+    //   const jsonResponse = await fetch(`${_URL}login`, {
+    //     method: "POST", // Метод POST для отправки данных
+    //     headers: {
+    //       "Content-Type": "application/json", // Тип содержимого
+    //     },
+    //     body: JSON.stringify({
+    //       email: this.loginInputEmailEl.value, // Передаем email из поля ввода
+    //       password: this.loginInputPasswordEl.value, // Передаем пароль из поля ввода
+    //       device_name: "windows", // Указываем устройство
+    //     }),
+    //   });
+
+    // } catch (error) {
+    //   console.log(error); // Логируем ошибку сети
+    //   alert("Произошла ошибка. Попробуйте снова позже."); // Выводим сообщение о сетевой ошибке
+    // }
   }
 }
