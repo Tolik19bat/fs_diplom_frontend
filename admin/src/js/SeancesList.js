63d031a27f190866ec93947840a6513a1a1d56e3
Anatoly Batov <play.hoffa@gmail.com>
2025-04-02 11:09:46 +0400

    SeancesList

--- admin/src/js/SeancesList.js
+++ admin/src/js/SeancesList.js
@@ -1,75 +1,87 @@
+// Импортируем класс HallSeances, который отвечает за управление сеансами в зале
 import HallSeances from "./HallSeances.js";
 
+// Экспортируемый класс для управления списком сеансов
 export default class SeancesList {
+  /**
+   * Конструктор класса
+   * @param {Array} movies - Массив фильмов
+   * @param {Array} halls - Массив залов (по умолчанию пустой)
+   */
   constructor(movies, halls = []) {
-    // Сохраняем список фильмов и создаём массивы для залов и их сеансов.
-    this.movies = movies;
-    this.halls = halls;
-    this.hallsSeances = [];
-    this.init();
-    // Логирование созданного объекта
-    //  console.log("Создан новый объект SeancesList:", this);
+    this.movies = movies; // Сохраняем список фильмов
+    this.halls = halls; // Сохраняем список залов
+    this.hallsSeances = []; // Создаём пустой массив для объектов HallSeances
+    this.init(); // Инициализируем класс
   }
 
-  // Метод инициализации, вызывающий привязку событий к элементам DOM.
+  /**
+   * Метод инициализации
+   * Привязывает события к DOM и вызывает отрисовку сеансов
+   */
   init() {
-    this.bindToDom();
-    this.getHallsSeances(this.halls);
-    this.renderHallsSeances();
+    this.bindToDom(); // Привязываем обработчики событий к элементам DOM
+    this.getHallsSeances(this.halls); // Создаём объекты HallSeances для залов
+    this.renderHallsSeances(); // Отрисовываем сеансы в интерфейсе
   }
 
-  // Метод привязки элементов и событий DOM.
+  /**
+   * Метод привязки событий к элементам DOM
+   */
   bindToDom() {
-    // Получаем главный элемент с классом "main".
+    // Получаем основной контейнер страницы
     this.mainEl = document.querySelector(".main");
 
-    // Привязываем обработчик события "updateHall" к контексту текущего экземпляра.
+    // Привязываем обработчик события "updateHall" к текущему контексту
     this.hallUpdateHandler = this.hallUpdateHandler.bind(this);
-    this.mainEl.addEventListener("updateHall", this.hallUpdateHandler);
+    this.mainEl.addEventListener("updateHall", this.hallUpdateHandler); // Слушаем обновление залов
 
-    // Привязываем обработчик события "updateHallsSeances".
+    // Привязываем обработчик события "updateHallsSeances" к текущему контексту
     this.updateHallsSeances = this.updateHallsSeances.bind(this);
-    this.mainEl.addEventListener("updateHallsSeances", this.updateHallsSeances);
+    this.mainEl.addEventListener("updateHallsSeances", this.updateHallsSeances); // Слушаем обновление сеансов
 
-    // Получаем контейнер для отображения сеансов.
+    // Получаем контейнер, в который будут добавляться сеансы
     this.containerEl = document.querySelector(".conf-step__seances");
   }
 
-  // Обработчик события "updateHall", обновляющий список залов и сеансов.
+  /**
+   * Обработчик события обновления залов
+   * @param {Event} e - Событие, содержащее новые данные о залах
+   */
   hallUpdateHandler(e) {
-    // Получаем массив залов из данных события.
-    this.halls = e.detail.data;
-    // Инициализируем сеансы для полученных залов.
-    this.getHallsSeances(this.halls);
-    // Отрисовываем обновлённые сеансы.
-    this.renderHallsSeances();
+    this.halls = e.detail.data; // Обновляем список залов
+    this.getHallsSeances(this.halls); // Пересоздаём объекты HallSeances
+    this.renderHallsSeances(); // Перерисовываем сеансы
   }
 
-  // Метод создания объектов HallSeances для каждого зала.
+  /**
+   * Метод создания объектов HallSeances для каждого зала
+   * @param {Array} halls - Массив залов
+   */
   getHallsSeances(halls) {
-    // Очищаем текущий список сеансов залов.
-    this.hallsSeances = [];
-    // Для каждого зала создаём новый объект HallSeances и добавляем его в массив.
+    this.hallsSeances = []; // Очищаем массив объектов HallSeances
     halls.forEach((hall) => {
-      const hallSeances = new HallSeances(hall, this.movies);
-      this.hallsSeances.push(hallSeances);
+      const hallSeances = new HallSeances(hall, this.movies); // Создаём объект HallSeances
+      this.hallsSeances.push(hallSeances); // Добавляем его в массив
     });
   }
 
-  // Метод отрисовки всех сеансов для залов.
+  /**
+   * Метод отрисовки всех сеансов в DOM
+   */
   async renderHallsSeances() {
-    // Очищаем контейнер перед добавлением новых элементов.
-    this.containerEl.innerHTML = "";
-    // Добавляем элементы сеансов для каждого зала.
+    this.containerEl.innerHTML = ""; // Очищаем контейнер перед добавлением новых элементов
     for (const item of this.hallsSeances) {
-      const element = await item.getSeancesHallElement();
-      this.containerEl.appendChild(element);
+      const element = await item.getSeancesHallElement(); // Получаем DOM-элемент с сеансами
+      this.containerEl.appendChild(element); // Добавляем его в контейнер
     }
   }
 
-  // Обновляет список сеансов, вызывая соответствующие методы.
+  /**
+   * Метод обновления списка сеансов (пересоздаёт объекты и перерисовывает их)
+   */
   updateHallsSeances() {
-    this.getHallsSeances(this.halls);
-    this.renderHallsSeances();
+    this.getHallsSeances(this.halls); // Обновляем объекты HallSeances
+    this.renderHallsSeances(); // Перерисовываем сеансы в интерфейсе
   }
 }
