108c38cddd2ee7ffc5fe622c86d0519570f1c958
Anatoly Batov <play.hoffa@gmail.com>
2025-04-02 11:02:43 +0400

    SeanceGrid

--- admin/src/js/SeanceGrid.js
+++ admin/src/js/SeanceGrid.js
@@ -1,68 +1,79 @@
-// import { _URL } from "./app.js";
-import AddMovieModal from "./AddMovieModal.js";
-import PosterList from "./PosterList.js";
-import PosterModal from "./PosterModal.js";
-import SeancesList from "./SeancesList.js";
-import SeanceModal from "./SeanceModal.js";
-import Fetch from "./Fetch.js";
+// Импортируем модули, необходимые для работы класса
+import AddMovieModal from "./AddMovieModal.js"; // Модальное окно добавления фильма
+import PosterList from "./PosterList.js"; // Список постеров фильмов
+import PosterModal from "./PosterModal.js"; // Модальное окно для отображения постера
+import SeancesList from "./SeancesList.js"; // Список сеансов
+import SeanceModal from "./SeanceModal.js"; // Модальное окно для настройки сеансов
+import Fetch from "./Fetch.js"; // Модуль для выполнения HTTP-запросов
 
+// Класс для управления сеткой сеансов
 export default class SeanceGrid {
+  /**
+   * Конструктор класса
+   * @param {Array} halls - Массив залов, передаваемый в качестве аргумента
+   */
   constructor(halls = []) {
-    this.halls = halls; // Инициализируем массив залов
-    this.init(); // Инициализируем экземпляр класса
-    // Логирование созданного объекта
-    // console.log("Создан новый объект SeqanceGrid:", this);
+    this.halls = halls; // Сохраняем переданный массив залов
+    this.init(); // Вызываем метод инициализации
   }
 
-  // Метод для инициализации логики класса
+  /**
+   * Метод инициализации логики класса
+   * Получает список фильмов, инициализирует модальные окна и списки
+   */
   init() {
-    this.bindToDom(); // Связываем DOM-элементы с методами
-    // Получаем список фильмов и инициализируем модальные окна и списки
+    this.bindToDom(); // Привязываем методы к DOM-элементам
+
+    // Загружаем список фильмов с сервера
     this.getMovies().then((movies) => {
-      console.log("Полученные фильмы:", JSON.stringify(movies, null, 2)); // Выводим JSON с отступами
-      AddMovieModal.init(); // Инициализация окна добавления фильма
+      AddMovieModal.init(); // Инициализируем модальное окно добавления фильма
       this.posterList = new PosterList(movies); // Создаем список постеров фильмов
-      PosterModal.init(); // Инициализация модального окна постера
+      PosterModal.init(); // Инициализируем модальное окно постера
       this.seancesList = new SeancesList(movies, this.halls); // Создаем список сеансов
-      SeanceModal.init(); // Инициализация модального окна сеанса
+      SeanceModal.init(); // Инициализируем модальное окно настройки сеанса
     });
   }
 
-  // Метод для связывания DOM-элементов
+  /**
+   * Метод привязки событий к элементам DOM
+   */
   bindToDom() {
-    this.mainEl = document.querySelector(".main"); // Находим основной элемент
-    this.updateHandler = this.updateHandler.bind(this); // Привязываем обработчик обновления
+    // Получаем главный контейнер страницы
+    this.mainEl = document.querySelector(".main");
+
+    // Привязываем обработчик обновления зала к текущему экземпляру
+    this.updateHandler = this.updateHandler.bind(this);
     this.mainEl.addEventListener("updateHall", this.updateHandler); // Добавляем слушатель события обновления зала
-    this.addMovieBtnEl = document.querySelector(".add-movie-btn"); // Находим кнопку добавления фильма
-    this.onClickAddMovieBtn = this.onClickAddMovieBtn.bind(this); // Привязываем обработчик клика по кнопке
-    this.addMovieBtnEl.addEventListener("click", this.onClickAddMovieBtn); // Добавляем слушатель клика по кнопке
+
+    // Получаем кнопку "Добавить фильм"
+    this.addMovieBtnEl = document.querySelector(".add-movie-btn");
+
+    // Привязываем обработчик клика к кнопке
+    this.onClickAddMovieBtn = this.onClickAddMovieBtn.bind(this);
+    this.addMovieBtnEl.addEventListener("click", this.onClickAddMovieBtn); // Добавляем слушатель клика
   }
 
-  // Обработчик события клика по кнопке добавления фильма
+  /**
+   * Обработчик клика по кнопке "Добавить фильм"
+   */
   onClickAddMovieBtn() {
-    AddMovieModal.showModal(); // Показываем модальное окно добавления фильма
+    AddMovieModal.showModal(); // Открываем модальное окно добавления фильма
   }
 
-  // Обработчик обновления зала
+  /**
+   * Обработчик события обновления зала
+   * @param {Event} e - Объект события
+   */
   updateHandler(e) {
-    this.halls = e.detail.data; // Получаем данные залов из события
-    this.activeHallId = e.detail.id; // Получаем идентификатор активного зала
+    this.halls = e.detail.data; // Обновляем массив залов
+    this.activeHallId = e.detail.id; // Сохраняем идентификатор активного зала
   }
 
-  // Асинхронный метод для получения списка фильмов
+  /**
+   * Асинхронный метод получения списка фильмов с сервера
+   * @returns {Promise<Array>} - Промис с массивом фильмов
+   */
   async getMovies() {
-    return await Fetch.send("GET", "movie"); // Отправляем асинхронный GET-запрос на сервер для получения списка фильмов
-
-    // const token = localStorage.getItem("token"); // Получаем токен из локального хранилища
-    // try {
-    //   const jsonResponse = await fetch(`${_URL}movie`, {
-    //     method: "GET",
-    //     headers: { Authorization: `Bearer ${token}` }, // Добавляем токен в заголовки запроса
-    //   });
-    //   const response = await jsonResponse.json(); // Преобразуем ответ в JSON
-    //   return response; // Возвращаем список фильмов
-    // } catch (error) {
-    //   console.error(error); // Выводим ошибку в консоль при возникновении
-    // }
+    return await Fetch.send("GET", "movie"); // Отправляем GET-запрос для получения списка фильмов
   }
 }
